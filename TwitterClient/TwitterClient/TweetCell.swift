083c463654a13c7c038e38fac415d681359326f1
Jayb967 <rio@Rios-MacBook-Pro.local>
2017-03-22 15:52:18 -0700

    did Wednesday lecture

---
+++ TwitterClient/TwitterClient/TweetCell.swift
@@ -0,0 +1,16 @@
+//
+//  TweetCell.swift
+//  TwitterClient
+//
+//  Created by Rio Balderas on 3/22/17.
+//  Copyright © 2017 Jay Balderas. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+   
+   @IBOutlet weak var tweetLabel: UILabel!
+   @IBOutlet weak var tweetUserLabel: UILabel!
+ 
+}
