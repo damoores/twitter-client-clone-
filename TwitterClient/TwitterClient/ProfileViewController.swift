db26f954da444a45d01d3caa996dfde7f128a7b1
Jayb967 <rio@rios-macbook-pro.local>
2017-03-26 21:45:22 -0700

    sunday twitter client

--- TwitterClient/TwitterClient/ProfileViewController.swift
+++ TwitterClient/TwitterClient/ProfileViewController.swift
@@ -17,6 +17,7 @@ class ProfileViewController: UIViewController {
       super.viewDidLoad()
       
       
+      
       // Do any additional setup after loading the view.
    }
    var profile : User?
